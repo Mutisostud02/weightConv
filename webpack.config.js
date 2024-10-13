f2f91366a0b7841cd00e665c04897df6f0648535
Mutisostud02 <https://github.com/Mutisostud02>
2024-10-13 21:03:19 +0300

    Removed unnecessary settings.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const toml = require('toml');
-// const yaml = require('yamljs');
-// const json5 = require('json5');
+const json5 = require('json5');
 
 module.exports = {
     mode: 'development',
@@ -48,28 +46,6 @@ module.exports = {
           {
             test: /\.(woff|woff2|eot|ttf|otf)$/i,
             type: 'asset/resource',
-          },
-          {
-             test: /\.(csv|tsv)$/i,
-             use: ['csv-loader'],
-          },
-          {
-             test: /\.xml$/i,
-             use: ['xml-loader'],
-          },
-          {
-             test: /\.toml$/i,
-             type: 'json',
-             parser: {
-               parse: toml.parse,
-             },
-          },
-          {
-             test: /\.yaml$/i,
-             type: 'json',
-             parser: {
-              parse: yaml.parse,
-              },
           },
           {
              test: /\.json5$/i,
